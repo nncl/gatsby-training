b41e2e319b22c1a24b17767645de1c7e06a090b3
= <nncl@live.com>
2020-04-09 23:14:33 -0300

    Adding styled components

---
+++ src/components/test/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import { Title } from "./styles"
+
+export default function Test({children}) {
+  return <Title>{children}</Title>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,12 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Test from "../components/test"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Test>Oh yeah!</Test>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
